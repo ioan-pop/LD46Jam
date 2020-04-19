0bb123ae1ff4e857efa9422f9c2fee0e5836752a
ipop1 <ioan.pop@live.com>
2020-04-19 01:23:56 -0400

    MASTER: Added religion name to the main scene

--- Assets/Scripts/UIController.cs
+++ Assets/Scripts/UIController.cs
@@ -1,18 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Text religionNameText;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        if(PlayerDetailsManager.instance != null) {
+            religionNameText.text = PlayerDetailsManager.instance.religionName;
+        }
     }
 }
